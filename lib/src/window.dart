bcd5ac8a123fcbae4c73623aed3fa7f8c1ed3c65
phantamanta44 <evandalong@gmail.com>
2016-10-27 21:31:11 -0500

    More utility stuff

--- lib/src/window.dart
+++ lib/src/window.dart
@@ -3,27 +3,37 @@ part of curses_abst;
 abstract class Window {
   Matrix<Cell> _cells;
 
-  Window(int x, int y) {
-    _cells = new Matrix.supplied(x, y, () => new Cell());
+  Window(int width, int height) {
+    // TODO Implement
   }
+
+  Window._init(int x, int y, Function cellFactory) {
+    _cells = new Matrix.supplied(x, y, cellFactory);
+  }
+
+  get width => _cells.width;
+
+  get height => _cells.height;
 
   Cell getCell(int x, int y) => _cells.get(x, y);
 
   void write(int x, int y, String text) {
-    if (x + text.length > _cells.width)
+    if (x + text.length > width)
       throw new IndexError(x + text.length, this);
     for (int i = 0; i < text.length; i++)
       getCell(x + i, y).text = text[i];
   }
 
+  void clear() => regionOf(0, 0, width, height).forEach((c) => c.clear());
+
   Iterable<Cell> regionOf(int x, int y, int width, int height) sync* {
-    if (x + width > _cells.width)
+    if (x + width > width)
       throw new IndexError(width, this);
-    if (y + height > _cells.height)
+    if (y + height > height)
       throw new IndexError(height, this);
     for (int yOff = 0; yOff < height; yOff++) {
       for (int xOff = 0; xOff < width; xOff++)
-        yield _cells.get(x + xOff, y + yOff);
+        yield getCell(x + xOff, y + yOff);
     }
   }
 
@@ -37,7 +47,11 @@ abstract class Cell {
 
   get text => _text;
 
-  void clear() {
+  void clearText() {
     text = "";
+  }
+
+  void clear() {
+    // TODO Implement
   }
 }
